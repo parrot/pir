d00c90b1edb04e31ec55e55cc4ba611588b0be62
Vasily Chekalkin <bacek@bacek.com>
2010-05-20 19:15:41 +1000

    Start building grammar

--- src/Compiler/Grammar.pm
+++ src/Compiler/Grammar.pm
@@ -8,18 +8,18 @@ class PIR::Grammar is HLL::Grammar;
 
 # Top-level rules.
 rule TOP {
-    <compilation_unit>
+    <compilation_unit>*
     [ $ || <panic: "Confused"> ]
 }
 
 proto token compilation_unit { <...> }
 
 token compilation_unit:sym<sub> {
-    '.sub'
-    .'end'
+    '.sub' <.ws> <subname> <.nl>
+    '.end' <.terminator>
 }
 
-token compilation_unit:sym<pragma> { <pragma> }
+#token compilation_unit:sym<pragma> { }
 
 rule statement_list {
     | $
@@ -36,19 +36,35 @@ token terminator {
     | \n
 }
 
+token subname {
+    [
+    | <string_constant>
+    | <ident>
+    ]
+}
+
+
 token pod_comment {
     ^^ '=' <pod_directive>
     .* \n
     ^^ '=cut'
 }
 
+# There is no iterpolation of strings in PIR
+proto token string_constant { <...> }
+token string_constant:sym<apos> { <?[']> <quote_EXPR: ':q'>  }
+token string_constant:sym<dblq> { <?["]> <quote_EXPR: ':q'> }
+
 # Don't be very strict on pod comments (for now?)
 token pod_directive { <ident> }
+
+token nl { \v+ }
 
 # Any "whitespace" including pod comments
 token ws {
     <!ww>
-        [ \v+
+        [
+        | ^^ \v+ # newlines accepted only by themselfs
         | '#' \N*
         | ^^ <.pod_comment>
         | \h+
