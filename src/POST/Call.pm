1bd734611ed849c0891fb590015c9fe3312f856a
Vasily Chekalkin <bacek@bacek.com>
2010-06-19 22:42:55 +1000

    Add POST.Call.invocant

--- src/POST/Call.pm
+++ src/POST/Call.pm
@@ -6,15 +6,17 @@ POST node to represent single PCC call (including .tailcall and .return)
 
 =end Description
 
-our multi method name($param) { self.attr('name', $param, 1); }
-our multi method name()       { self.attr('name', undef,  0); }
+our multi method name($param)       { self.attr('name', $param, 1); }
+our multi method name()             { self.attr('name', undef,  0); }
 
-our multi method params($param)  { self.attr('params', $param, 1); }
-our multi method params()        { self.attr('params', undef,  0); }
+our multi method params($param)     { self.attr('params', $param, 1); }
+our multi method params()           { self.attr('params', undef,  0); }
 
-our multi method results($param) { self.attr('results', $param, 1); }
-our multi method results()       { self.attr('results', undef,  0); }
+our multi method results($param)    { self.attr('results', $param, 1); }
+our multi method results()          { self.attr('results', undef,  0); }
 
+our multi method invocant($param)   { self.attr('invocant', $param, 1); }
+our multi method invocant()         { self.attr('invocant', undef,  0); }
 
 
 # vim: ft=perl6
