ef25afcf83bc8afb630a69d0f640a1ff8e5a0ef9
Vasily Chekalkin <bacek@bacek.com>
2010-06-20 20:19:14 +1000

    Add "rationale" for POST::Compiler.pbc. sorear++ for motivating.

--- src/PIR/Compiler.pm
+++ src/PIR/Compiler.pm
@@ -6,6 +6,26 @@ INIT {
     PIR::Compiler.parseactions(PIR::Actions);
 }
 
+=begin
+Emit PBC file.
+
+Long explanation about it:
+Currently there is no way in Parrot to generate Packfile and attach it
+to Interp via existing API. This function is partially hack to generate
+serialized PBC, load it back and execute it (modulus fix for TT#1685).
+
+Best way to deal with such issues is:
+1. Switch Interp to use Packfile* PMC internally.
+2. Add API calls to attach freshly generated Packfile to current Interp.
+
+Quick "fix" can be:
+1. Add "PackFile_unpack_string" function which will accept STRING.
+2. Expose this function via Interp (or Packfile PMC method).
+
+Kind of wishful thinking, but we can fix it.
+
+=end
+
 our method pbc($post, *%adverbs) {
     #pir::trace(4);
     my $packfile := POST::Compiler.pbc($post, %adverbs);
