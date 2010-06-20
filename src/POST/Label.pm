4d6428059ff97911520a3cc4d798eff99facea5e
Vasily Chekalkin <bacek@bacek.com>
2010-06-21 07:10:12 +1000

    Add Label.declared

--- src/POST/Label.pm
+++ src/POST/Label.pm
@@ -1,3 +1,10 @@
 module POST::Label;
 
+=item C<declared>
+Set to 1 of we did see label: inside.
+
+our multi method declared($param) { self.attr('declared', $param, 1); }
+our multi method declared()       { self.attr('declared', undef,  0); }
+
+
 # vim: ft=perl6
