e3165e0b9e950db939a8e9199e29b3a58ba39dda
Vasily Chekalkin <bacek@bacek.com>
2010-06-21 21:33:26 +1000

    Add POST::Label.position

--- src/POST/Label.pm
+++ src/POST/Label.pm
@@ -6,5 +6,9 @@ Set to 1 of we did see label: inside.
 our multi method declared($param) { self.attr('declared', $param, 1); }
 our multi method declared()       { self.attr('declared', undef,  0); }
 
+# Position in bytecode segment.
+our multi method position($param) { self.attr('position', $param, 1); }
+our multi method position()       { self.attr('position', undef,  0); }
+
 
 # vim: ft=perl6
