3570a3868812893f208334c0121652f5111a3310
Christoph Otto <christoph_git@mksig.org>
2010-06-22 18:42:09 -0700

    first shot at a POST::Key implementation

---
+++ src/POST/Key.pm
@@ -0,0 +1,13 @@
+class POST::Key is POST::Value;
+
+=begin Description
+A POST::Key represents a single key such as ['Foo';'Bar';'Buz'].
+=end Description
+
+our multi method keys($param) { self.attr('keys', $param, 1); }
+our multi method keys()       { self.attr('keys', undef,  0); }
+
+#keys represented in PIR are constant
+our multi method type()       { 'kc'; }
+
+# vim: ft=perl6
