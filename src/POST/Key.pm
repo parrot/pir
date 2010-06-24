dcea987fb1555fea36409231b893abbc2a9c910e
Christoph Otto <christoph_git@mksig.org>
2010-06-24 10:06:48 -0700

    expand docs to make POST::Key a little less mysterious

--- src/POST/Key.pm
+++ src/POST/Key.pm
@@ -1,7 +1,9 @@
 class POST::Key is POST::Value;
 
 =begin Description
-A POST::Key represents a single key such as ['Foo';'Bar';'Buz'].
+This stub class exists to give a unique class for mmd in POST::Compiler::to_pbc.
+
+A POST::Key represents a single (possibly compound) key such as [1] or ['Foo';'Bar';'Buz'].
 =end Description
 
 # vim: ft=perl6
