acc6a848bc2b5bc9a99e288d71a2fad421ab3eec
Vasily Chekalkin <bacek@bacek.com>
2010-06-29 19:28:55 +1000

    Add POST::File.sub method.

--- src/POST/File.pm
+++ src/POST/File.pm
@@ -4,6 +4,23 @@ class POST::File is POST::Node;
 Single compiled file. Use to store global constants, e.g. Subs and .global_const
 =end Description
 
+=item C<sub($name, $value?)
+Get or set Sub declared in file.
+
+our method sub($name, $value?) {
+    my %subs := self<subs>;
+    unless %subs {
+        self<subs> := hash();
+        %subs := self<subs>;
+    }
+
+    if $value {
+        %subs{$name} := $value;
+    }
+
+    %subs{$name};
+}
+
 
 
 # vim: ft=perl6
