9891ac250bd817be41027354db0693fc3b22c13e
Vasily Chekalkin <bacek@bacek.com>
2010-07-01 21:15:47 +1000

    Add POST::Key.Str method for stringification.

--- src/POST/Key.pm
+++ src/POST/Key.pm
@@ -1,9 +1,13 @@
 class POST::Key is POST::Value;
 
 =begin Description
-This stub class exists to give a unique class for mmd in POST::Compiler::to_pbc.
+This stub class (almost) exists to give a unique class for mmd in POST::Compiler::to_pbc.
 
 A POST::Key represents a single (possibly compound) key such as [1] or ['Foo';'Bar';'Buz'].
 =end Description
+
+our method Str() {
+    '[' ~ @(self).map(sub($p) { $p<value> }).join(';') ~ ']';
+}
 
 # vim: ft=perl6
